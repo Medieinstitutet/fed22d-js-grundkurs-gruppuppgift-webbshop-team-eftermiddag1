8e367010ca8d6ebb2c2bf8bc48ae59063775948a
Malin Dahlquist <m-dahlquist@hotmail.com>
2022-11-24 22:46:10 +0100

    Added JavaScript to form

--- script.js
+++ script.js
@@ -24,25 +24,47 @@ function summaryOpen() {
   summary.classList.toggle('open');
 }
 
-// Kort eller faktura
+// Kort eller faktura samt beställningsformulär
 const invoiceButton = document.querySelector('#invoice');
 const invoiceFields = document.querySelector('.invoice');
 const cardButton = document.querySelector('#card');
 const cardFields = document.querySelector('.card');
+const form = document.getElementById('form');
+const clearOrderButton = document.getElementById('clearOrderButton');
+const submitButton = document.getElementById('submitButton');
+const ssnField = document.getElementById('ssn');
+const cardNumberField = document.getElementById('cardNumber');
+const monthField = document.getElementById('month');
+const yearField = document.getElementById('year');
+const cvcField = document.getElementById('cvc');
 
 invoiceButton.addEventListener('click', showInvoiceFields);
 cardButton.addEventListener('click', showCardFields);
+clearOrderButton.addEventListener('click', resetForm);
+form.addEventListener('change', enableSubmitButtonOnValidForm);
 
 function showInvoiceFields() {
-  if (invoiceButton.checked) {
-    invoiceFields.style.display = 'block';
-    cardFields.style.display = 'none';
-  }
+  invoiceFields.style.display = 'block';
+  cardFields.style.display = 'none';
+  ssnField.required = true;
+  monthField.required = false;
+  yearField.required = false;
+  cvcField.required = false;
 }
 
 function showCardFields() {
-  if (cardButton.checked) {
-    cardFields.style.display = 'block';
-    invoiceFields.style.display = 'none';
-  }
+  cardFields.style.display = 'block';
+  invoiceFields.style.display = 'none';
+  monthField.required = true;
+  yearField.required = true;
+  cvcField.required = true;
+  ssnField.required = false;
+}
+
+function resetForm() {
+  form.reset();
+}
+
+function enableSubmitButtonOnValidForm() {
+  submitButton.disabled = !form.checkValidity();
 }
